8dd81fa93f716f9349bc7cbe55c437fce4b03562
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-08-04 17:30:44 -0400

    apple-react

---
+++ apple-react/src/App.jsx
@@ -0,0 +1,29 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Alert from "./components/Alert";
+import First from "./components/First";
+import Second from "./components/Second";
+import Third from "./components/Third";
+import Fourth from "./components/Fourth";
+import Fifth from "./components/Fifth";
+import Six from "./components/Six";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Alert />
+      <First />
+      <Second />
+      <Third />
+      <Fourth />
+      <Fifth />
+      <Six />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
