6c67234cf833b8090dcdaf6151b81d34ee33151e
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-08-23 10:51:40 -0400

    change

--- apple-react/src/components/Footer.jsx
+++ apple-react/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import flag from "../images/icons/16.png";
 
 function Footer() {
   return (
-   <footer className="footer-wrapper">
+ <footer className="footer-wrapper">
 		<div className="container"> 
 			<div className="upper-text-container">
 				<p>1. Trade In: Trade‑in values vary. iPhone 11 and iPhone 11 Pro promotional pricing is after trade‑in of iPhone 8 Plus and iPhone X in good condition. Additional trade‑in values require purchase of a new iPhone, subject to availability and limits. Must be at least 18. Apple or its trade-in partners reserve the right to refuse or limit any Trade In transaction for any reason. In‑store trade‑in requires presentation of a valid, government-issued photo ID (local law may require saving this information). Sales tax may be assessed on full value of new iPhone. Additional terms from Apple or Apple’s trade-in partners may apply. Monthly pricing: Available to qualified customers and requires 0% APR, 24-month installment loan with Citizens One or Apple Card Monthly Installments and iPhone activation with AT&T, Sprint, T-Mobile, or Verizon. Taxes and shipping not included. Additional Apple Card Monthly Installments terms are in the <a href="https://www.goldmansachs.com/terms-and-conditions/Apple-Card-Customer-Agreement.pdf" target="_blank"> Customer Agreement</a>. Additional iPhone Payments terms are <a href="https://www.apple.com/legal/sales-support/iphoneinstallments_us/"> here</a>.</p>
@@ -121,12 +121,12 @@ function Footer() {
 					</ul>
 				</div>
 				<div className="footer-country  col-sm-12 order-1 col-md-4 order-md-2 text-md-right col-lg-2 order-lg-3">
-                <div className="flag-wrapper"><img src={flag} /></div> <div className="footer-country-name">United States</div> 
+					  <div className="flag-wrapper"><img src={flag}/></div> <div className="footer-country-name">United States</div> 
 				</div>
 			</div>
 		</div>
-	</footer> 
-  );
+		</footer> 
+  )
 }
 
 export default Footer;
