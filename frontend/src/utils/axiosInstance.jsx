d9fe5179492c184cc3a50c89d6093b0b5a9928c5
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-08-28 05:27:35 -0400

    Update axiosInstance.jsx

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
+  const url = import.meta.env.VITE_url;
 
 export const axiosInstance = axios.create({
-  baseURL: "https://apple-clone-backend-i7tq.onrender.com",
+  baseURL:url , 
 });
